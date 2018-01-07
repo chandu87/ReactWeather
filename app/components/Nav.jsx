a01bc26ac1493c73d8a75d2d607db5655de421b6
chandu <xlent@chandus-MacBook-Pro.local>
2018-01-07 23:53:38 +0100

    Navigation styling done

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,15 +3,40 @@ var React= require('react');
 var {Link}=require('react-router');
 
 var Nav = React.createClass({
+  onSearch:function(e){
+    e.preventDefault();
+    alert('not yet wired up');
+  },
   render:function(){
     return(
-      // <h2>adkasldjla</h2>
-      <div>
-        <Link to="/"> Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/examples">Examples</Link>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li><Link to="/"> Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/examples">Examples</Link></li>
 
-    </div>
+          </ul>
+
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+              <ul className="menu">
+                <li>
+                  <input type="search" placeholder="Search weather"/>
+                </li>
+                <li>
+                  <input type="submit" className="button" value="Get Weather"/>
+                </li>
+
+              </ul>
+          </form>
+        </div>
+
+
+
+     </div>
     );
   }
 });
