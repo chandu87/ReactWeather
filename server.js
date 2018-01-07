31da73b8fad17f25911b3e60c5e5fa270d182fa4
chandu <xlent@chandus-MacBook-Pro.local>
2018-01-07 01:02:08 +0100

    server.js https added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT= process.env.PORT || 3000;
 
 
 app.use(function(req, res, next){
-	if(req.headers['x-forwarded-porto'] === 'http'){
+	if(req.headers['x-forwarded-porto'] === 'https'){
   next();
 	}
 	else{
