d0d9ae8eead7ab69a3b925e4f3cfa1e441bc68eb
chandu <chandrasr.palle@gmail.com>
2018-01-09 13:05:13 +0100

    About page styling done

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,19 +1,25 @@
 var React= require('react');
 
-var About= React.createClass({
-  render:function(){
+var About= (props)=> {
     return(
       <div>
-        <h4>About Component</h4>
-        <p>About weather app... it is written in javascript!!</p>
+        <h1 className="text-center">About</h1>
+        <p>Application for searching weather in your city.
+          This application developed as part of learning
+          Web development with React javascript.</p>
+          <p>The tools used in this project are listed below</p>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Other</li>
+          <li><a href="https://reactjs.org/">React JS </a>
+          - A Javascript library for building interfaces</li>
+          <li><a href="http://openweathermap.org/">OpenWeatherMap </a>
+          - It is an opensource api for weather data resource.
+          Simple and clear API, Current conditions and weather
+          forecast for morethan 200000 cities.
+        </li>
         </ul>
 
       </div>
-    );
-  }
-});
+    )
+  };
+
 module.exports=About;
